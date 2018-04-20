4c5e9f20d789b9bb4a375b78f0f469ff6718be8f
hardikk08 <hardiknagda12@gmail.com>
2018-04-20 23:10:42 +0530

    express routing

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ var app = express();
 var path = require('path');
 
 app.use(express.static(path.join(__dirname)));
-app.use("/styles", express.static(__dirname));
-app.use("/images", express.static(__dirname + '/images'));
-app.use("/scripts", express.static(__dirname + '/scripts'));
+// app.use("/styles", express.static(__dirname));
+// app.use("/images", express.static(__dirname + '/images'));
+// app.use("/scripts", express.static(__dirname + '/scripts'));
+//
 
-// viewed at based directory http://localhost:8080/
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
+// app.get(/.*/, function (req, res) {
+//   res.sendFile(path.join(__dirname + '/views/index.html'));
+// });
+
+app.all('*', function(req, res) {
+  res.redirect("https://my-weatherapp1.herokuapp.com/");
 });
 
-// add other routes below
-// app.get('/about', function (req, res) {
-//   res.sendFile(path.join(__dirname + '/views/home.html'));
-// });
 
 app.listen(process.env.PORT || 8080);
